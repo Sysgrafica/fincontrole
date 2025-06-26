6de71a6cca4fb780a20a8a3e402bedc069cfd9c1
grafica <guimoraes19999@gmail.com>
2025-06-26 12:07:42 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+// IMPORTANTE: Este arquivo deve ser adicionado ao .gitignore para não ser enviado ao repositório
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC_9d4m0crliSZvAd4py5exJixQFOtOvD8",
+    authDomain: "fincontrole-70ec4.firebaseapp.com",
+    projectId: "fincontrole-70ec4",
+    storageBucket: "fincontrole-70ec4.appspot.com",
+    messagingSenderId: "250535426531",
+    appId: "1:250535426531:web:6f509f1ee9914a550188d6"
+}; 
